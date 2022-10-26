6323cb9ce8e8004b5051971c0a7255037b86b24c
Mov305 <abd3052001@gmail.com>
2022-10-27 00:07:21 +0200

    fixed linters

--- morse_code.rb
+++ morse_code.rb
@@ -1,21 +1,21 @@
-$morse_pattern = { '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f', '--.' => 'g',
-                   '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n',
-                   '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's', '-' => 't', '..-' => 'u',
-                   '...-' => 'v', '.--' => 'w', '-..-' => 'x', '-.--' => 'y',
-                   '--..' => 'z', ' ' => ' ' }
+def decode_morse(morse_text)
+  morse_pattern = { '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f', '--.' => 'g',
+                    '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n',
+                    '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's', '-' => 't', '..-' => 'u',
+                    '...-' => 'v', '.--' => 'w', '-..-' => 'x', '-.--' => 'y',
+                    '--..' => 'z', ' ' => ' ' }
 
-def decode_morse(morseText)
-  morse_array = morseText.split(/\s/).map { |i| $morse_pattern[i] }
+  morse_array = morse_text.split(/\s/).map { |i| morse_pattern[i] }
   text = ''
-  morse_array.each { |t|
-    if (t)
-      text = text + t
-    else
-      text = text + ' '
-    end
-  }
-  text2 = text.split().join(' ').upcase
-  return text2
+  morse_array.each do |t|
+    text = if t
+             text + t
+           else
+             "#{text} "
+           end
+  end
+  text.split.join(' ').upcase
 end
 
 puts(decode_morse('.-   -... --- -..-   ..-. ..- .-.. .-..          --- ..-.   .-. ..- -... .. . ...'))
+# => "A BOX FULL OF RUBIES"
